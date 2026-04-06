7048d6da1f237826c4486a5f607eaf55a899b7e8
dev global <balu.gounipuram@gmail.com>
2026-04-07 01:31:40 +0530

    HAS-001 - Project setup complete

---
+++ hospital/src/test/java/com/balu/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balu.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
